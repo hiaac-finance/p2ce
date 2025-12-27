13d08df4236c17c8671d27c17d379ac1f70597d8
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2025-12-26 22:53:04 -0300

    feat: better experiments interface

--- notebooks/experiment_lgbm.py
+++ notebooks/experiment_lgbm.py
@@ -26,243 +26,222 @@ from experiments_helper import (
 
 import argparse
 
+SEED = 0
 
-def run(dataset_name, n_samples=200):
-    SEED = 0
 
+def get_method(method_name, model, outlier_detection, X_train, Y_train, dataset):
     max_changes = 3
-    objective = "abs_diff"
 
+    if method_name == "p2ce_tree_abs_diff":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+        model_wrap = GeneralClassifier_Shap(
+            model,
+            outlier_detection,
+            X_train,
+            shap_explainer="tree",
+        )
+
+        method = P2CE(
+            action_set=action_set,
+            classifier=model_wrap,
+            compare="abs_diff",
+            max_changes=max_changes,
+            outlier_contamination=dataset.outlier_contamination,
+            estimate_outlier=True,
+            time_limit=np.inf,
+        )
+
+    elif method_name == "mapocam_tree_abs_diff":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+        for feat in action_set:
+            feat.flip_direction = 1
+            feat.update_grid()
+        model_wrap = TreeClassifier(
+            classifier=model,
+            X=X_train,
+            y=Y_train,
+            use_predict_max=True,
+            clf_type="lightgbm",
+        )
+
+        method = MAPOCAM(
+            action_set=action_set,
+            model=model_wrap,
+            criteria="abs_diff",
+            max_changes=max_changes,
+        )
+    elif method_name == "nice":
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = Nice(
+            X_train,
+            Y_train,
+            model=model,
+            cat_features=dataset.categoric_features,
+        )
+    elif method_name == "dice":
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = Dice(
+            X_train,
+            Y_train,
+            model,
+            n_cfs=1,
+            mutable_features=dataset.mutable_features,
+            continuous_features=dataset.continuous_features,
+        )
+    elif method_name == "p2ce_tree_multi":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+
+        model_wrap = GeneralClassifier_Shap(
+            model,
+            outlier_detection,
+            X_train,
+            shap_explainer="tree",
+        )
+
+        # setting multiple criteria
+        range_calc = RangeCalculator(action_set)
+
+        def compare_call(pivot):
+            criteria_list = [
+                MaxDistCriterion(
+                    pivot,
+                    range_calc,
+                ),
+                NumberChangesCriterion(pivot),
+                AbsDiffCriterion(pivot, range_calc),
+            ]
+            return MultiCriterion(criteria_list, pivot)
+
+        method = P2CE(
+            action_set=action_set,
+            classifier=model_wrap,
+            compare=compare_call,
+            max_changes=max_changes,
+            outlier_contamination=dataset.outlier_contamination,
+            estimate_outlier=True,
+            time_limit=np.inf,
+        )
+    elif method_name == "mapocam_tree_multi":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+
+        for feat in action_set:
+            feat.flip_direction = 1
+            feat.update_grid()
+
+        model_wrap = TreeClassifier(
+            classifier=model,
+            X=X_train,
+            y=Y_train,
+            use_predict_max=True,
+            clf_type="lightgbm",
+        )
+
+        # setting multiple criteria
+        range_calc = RangeCalculator(action_set)
+
+        def compare_call(pivot):
+            criteria_list = [
+                MaxDistCriterion(
+                    pivot,
+                    range_calc,
+                ),
+                NumberChangesCriterion(pivot),
+                AbsDiffCriterion(pivot, range_calc),
+            ]
+            return MultiCriterion(criteria_list, pivot)
+
+        method = MAPOCAM(
+            action_set=action_set,
+            model=model_wrap,
+            criteria=compare_call,
+            max_changes=max_changes,
+        )
+
+    elif method_name == "dice_multi":
+        n_cfs = 4
+
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = Dice(
+            X_train,
+            Y_train,
+            model,
+            n_cfs=n_cfs,
+            mutable_features=dataset.mutable_features,
+            continuous_features=dataset.continuous_features,
+        )
+
+    return method, model_wrap
+
+
+def run(dataset_name, method_name, n_samples=500):
+    # load dataset
     dataset, X_train, Y_train, model, outlier_detection, individuals = get_data_model(
         dataset_name, "LGBMClassifier_simple"
     )
     n = min(n_samples, len(individuals))
     individuals = individuals.sample(n=n, random_state=SEED)
     outlier_detection.contamination = dataset.outlier_contamination
-    action_set = get_action_set(dataset, X_train, default_step_size=0.05)
 
-    print("Running P2CE with ", dataset_name)
-
-    model_wrap = GeneralClassifier_Shap(
-        model,
-        outlier_detection,
-        X_train,
-        shap_explainer="tree",
+    # get method
+    method, model_wrap = get_method(
+        method_name, model, outlier_detection, X_train, Y_train, dataset
     )
 
-    method = P2CE(
-        action_set=action_set,
-        classifier=model_wrap,
-        compare=objective,
-        max_changes=max_changes,
-        outlier_contamination=dataset.outlier_contamination,
-        estimate_outlier=True,
-        time_limit=np.inf,
-    )
-
+    print(f"Running {method_name} with ", dataset_name)
+    # run experiments
     run_experiments(
         method,
         individuals=individuals,
         model=model_wrap,
-        output_file=f"../results/lgbm/{dataset}/p2ce_tree_{objective}.csv",
-    )
-
-    print("Running MAPOCAM with ", dataset_name)
-
-    action_set = get_action_set(dataset, X_train, default_step_size=0.05)
-    for feat in action_set:
-        feat.flip_direction = 1
-        feat.update_grid()
-
-    model_wrap = TreeClassifier(
-        classifier=model,
-        X=X_train,
-        y=Y_train,
-        use_predict_max=True,
-        clf_type="lightgbm",
-    )
-
-    method = MAPOCAM(
-        action_set=action_set,
-        model=model_wrap,
-        criteria=objective,
-        max_changes=max_changes,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset_name}/mapocam_tree_{objective}.csv",
-    )
-
-    print("Running NICE with ", dataset_name)
-
-    model_wrap = GeneralClassifier(
-        model,
-        outlier_detection,
-        X_train,
-    )
-
-    method = Nice(
-        X_train,
-        Y_train,
-        model=model,
-        cat_features=dataset.categoric_features,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset_name}/nice.csv",
-    )
-
-    print("Running DICE with ", dataset_name)
-
-    method = Dice(
-        X_train,
-        Y_train,
-        model,
-        n_cfs=1,
-        mutable_features=dataset.mutable_features,
-        continuous_features=dataset.continuous_features,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset_name}/dice.csv",
-    )
-
-    print("Running P2CE with multiple objectives with ", dataset_name)
-
-    action_set = get_action_set(dataset, X_train, default_step_size=0.05)
-
-    model_wrap = GeneralClassifier_Shap(
-        model,
-        outlier_detection,
-        X_train,
-        shap_explainer="tree",
-    )
-
-    # setting multiple criteria
-    range_calc = RangeCalculator(action_set)
-
-    def compare_call(pivot):
-        criteria_list = [
-            MaxDistCriterion(
-                pivot,
-                range_calc,
-            ),
-            NumberChangesCriterion(pivot),
-            AbsDiffCriterion(pivot, range_calc),
-        ]
-        return MultiCriterion(criteria_list, pivot)
-
-    method = P2CE(
-        action_set=action_set,
-        classifier=model_wrap,
-        compare=compare_call,
-        max_changes=max_changes,
-        outlier_contamination=dataset.outlier_contamination,
-        estimate_outlier=True,
-        time_limit=np.inf,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset}/p2ce_tree_multi.csv",
-    )
-
-    print("Running MAPOCAM with multiple objectives with ", dataset_name)
-
-    for feat in action_set:
-        feat.flip_direction = 1
-        feat.update_grid()
-
-    model_wrap = TreeClassifier(
-        classifier=model,
-        X=X_train,
-        y=Y_train,
-        use_predict_max=True,
-        clf_type="lightgbm",
-    )
-
-    # setting multiple criteria
-    range_calc = RangeCalculator(action_set)
-
-    def compare_call(pivot):
-        criteria_list = [
-            MaxDistCriterion(
-                pivot,
-                range_calc,
-            ),
-            NumberChangesCriterion(pivot),
-            AbsDiffCriterion(pivot, range_calc),
-        ]
-        return MultiCriterion(criteria_list, pivot)
-
-    method = MAPOCAM(
-        action_set=action_set,
-        model=model_wrap,
-        criteria=compare_call,
-        max_changes=max_changes,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset_name}/mapocam_tree_multi.csv",
-    )
-
-    print("Running DICE with multiple objectives with ", dataset_name)
-
-    n_cfs = 4
-
-    model_wrap = GeneralClassifier(
-        model,
-        outlier_detection,
-        X_train,
-    )
-
-    method = Dice(
-        X_train,
-        Y_train,
-        model,
-        n_cfs=n_cfs,
-        mutable_features=dataset.mutable_features,
-        continuous_features=dataset.continuous_features,
-    )
-
-    run_experiments(
-        method,
-        individuals=individuals,
-        model=model_wrap,
-        output_file=f"../results/lgbm/{dataset_name}/dice_multi.csv",
+        output_file=f"../results/lgbm/{dataset}/{method_name}.csv",
     )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dataset_id",
+        "--idx",
         type=int,
         default=0,
-        help="ID of the dataset to use (0: german, 1: taiwan, 2: adult)",
+        help="idx",
     )
     parser.add_argument(
         "--n_samples",
         type=int,
-        default=1_000,
+        default=500,
         help="Number of samples to use from the dataset",
     )
-    datasets = ["german", "taiwan", "adult"]
+    datasets = ["german", "taiwan", "adult", "acsincome", "homecredit"]
+    methods = [
+        "p2ce_tree_abs_diff",
+        "mapocam_tree_abs_diff",
+        "nice",
+        "dice",
+        "p2ce_tree_multi",
+        "mapocam_tree_multi",
+        "dice_multi",
+    ]
     args = parser.parse_args()
-    dataset_name = datasets[args.dataset_id]
+    dataset_id = args.idx // len(methods)
+    method_id = args.idx % len(methods)
 
-    run(dataset_name, n_samples=args.n_samples)
+    dataset_name = datasets[dataset_id]
+    method_name = methods[method_id]
+
+    if dataset_name == "homecredit":
+        run(dataset_name, method_name, n_samples=args.n_samples)
--- notebooks/experiment_mlp.py
+++ notebooks/experiment_mlp.py
@@ -17,214 +17,180 @@ import argparse
 
 SEED = 0
 
-def run(dataset_name, n_samples=200):
+def get_method(method_name, model, outlier_detection, X_train, Y_train, dataset):
     max_changes = 3
-    objective = "abs_diff"
-    dataset, X_train, Y_train, model, outlier_detection, individuals = get_data_model(dataset_name, "MLPClassifier")
+
+    if method_name == "p2ce_deep_abs_diff":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+        model_wrap = GeneralClassifier_Shap(
+            model,
+            outlier_detection,
+            X_train,
+            shap_explainer="deep_pipe",
+        )
+
+        method = P2CE(
+            action_set=action_set,
+            classifier=model_wrap,
+            compare="abs_diff",
+            max_changes=max_changes,
+            outlier_contamination=dataset.outlier_contamination,
+            estimate_outlier=True,
+            time_limit=np.inf,
+        )
+
+    elif method_name == "mapocam_deep_abs_diff":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+        for feat in action_set:
+            feat.flip_direction = 1
+            feat.update_grid()
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = MAPOCAM(
+            action_set=action_set,
+            model=model_wrap,
+            criteria="abs_diff",
+            max_changes=max_changes,
+        )
+    elif method_name == "nice":
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+        method = Nice(
+            X_train,
+            Y_train,
+            model=model,
+            cat_features=dataset.categoric_features,
+        )
+    elif method_name == "dice":
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = Dice(
+            X_train,
+            Y_train,
+            model,
+            n_cfs=1,
+            mutable_features=dataset.mutable_features,
+            continuous_features=dataset.continuous_features,
+        )
+    elif method_name == "p2ce_deep_multi":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+
+        model_wrap = GeneralClassifier_Shap(
+            model,
+            outlier_detection,
+            X_train,
+            shap_explainer="deep_pipe",
+        )
+
+        # setting multiple criteria
+        range_calc = RangeCalculator(action_set)
+
+        def compare_call(pivot):
+            criteria_list = [
+                MaxDistCriterion(
+                    pivot,
+                    range_calc,
+                ),
+                NumberChangesCriterion(pivot),
+                AbsDiffCriterion(pivot, range_calc),
+            ]
+            return MultiCriterion(criteria_list, pivot)
+
+        method = P2CE(
+            action_set=action_set,
+            classifier=model_wrap,
+            compare=compare_call,
+            max_changes=max_changes,
+            outlier_contamination=dataset.outlier_contamination,
+            estimate_outlier=True,
+            time_limit=np.inf,
+        )
+    elif method_name == "mapocam_deep_multi":
+        action_set = get_action_set(dataset, X_train, default_step_size=0.05)
+
+        for feat in action_set:
+            feat.flip_direction = 1
+            feat.update_grid()
+
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        # setting multiple criteria
+        range_calc = RangeCalculator(action_set)
+
+        def compare_call(pivot):
+            criteria_list = [
+                MaxDistCriterion(
+                    pivot,
+                    range_calc,
+                ),
+                NumberChangesCriterion(pivot),
+                AbsDiffCriterion(pivot, range_calc),
+            ]
+            return MultiCriterion(criteria_list, pivot)
+
+        method = MAPOCAM(
+            action_set=action_set,
+            model=model_wrap,
+            criteria=compare_call,
+            max_changes=max_changes,
+        )
+
+    elif method_name == "dice_multi":
+        n_cfs = 4
+
+        model_wrap = GeneralClassifier(
+            model,
+            outlier_detection,
+            X_train,
+        )
+
+        method = Dice(
+            X_train,
+            Y_train,
+            model,
+            n_cfs=n_cfs,
+            mutable_features=dataset.mutable_features,
+            continuous_features=dataset.continuous_features,
+        )
+
+    return method, model_wrap
+
+def run(dataset_name, method_name, n_samples=500):
+    # load dataset
+    dataset, X_train, Y_train, model, outlier_detection, individuals = get_data_model(
+        dataset_name, "MLPClassifier"
+    )
     n = min(n_samples, len(individuals))
-    individuals = individuals.sample(n = n, random_state=SEED)
+    individuals = individuals.sample(n=n, random_state=SEED)
     outlier_detection.contamination = dataset.outlier_contamination
-    action_set = get_action_set(dataset, X_train, default_step_size=0.05)
 
-    model_wrap = GeneralClassifier_Shap(
-        model,
-        outlier_detection,
-        X_train,
-        shap_explainer="deep_pipe",
+    # get method
+    method, model_wrap = get_method(
+        method_name, model, outlier_detection, X_train, Y_train, dataset
     )
 
-    print("Running P2CE with ", dataset_name)
-
-    method = P2CE(
-        action_set = action_set,
-        classifier = model_wrap,
-        compare = objective,
-        max_changes = max_changes if dataset_name != "taiwan" else 3,
-        outlier_contamination = dataset.outlier_contamination,
-        estimate_outlier=True,
-        time_limit=np.inf,
-    )
-
-    # run_experiments(
-    #     method,
-    #     individuals=individuals,
-    #     model=model_wrap,
-    #     output_file=f"../results/mlp/{dataset}/p2ce_deep_{objective}.csv"
-    # );
-
-    for feat in action_set:
-        feat.flip_direction = 1
-        feat.update_grid()
-
-    model_wrap = GeneralClassifier(
-        model,
-        outlier_detection,
-        X_train,
-    )
-
-    print("Running MAPOCAM with ", dataset_name)
-
-    method = MAPOCAM(
-        action_set = action_set,
-        model = model_wrap,
-        criteria = objective,
-        max_changes = max_changes,
-    )
-
-    # run_experiments(
-    #     method,
-    #     individuals=individuals,
-    #     model=model_wrap,
-    #     output_file=f"../results/mlp/{dataset_name}/mapocam_{objective}.csv"
-    # );
-
-
-    print("Running DICE with ", dataset_name)
-
-    method = Dice(
-        X_train,
-        Y_train,
-        model,
-        n_cfs = 1,
-        mutable_features = dataset.mutable_features,
-        continuous_features = dataset.continuous_features,
-    )
-
-    # run_experiments(
-    #     method,
-    #     individuals = individuals,
-    #     model = model_wrap,
-    #     output_file=f"../results/mlp/{dataset_name}/dice.csv"
-    # )
-
-
-    print("Running NICE with ", dataset_name)
-
-    method = Nice(
-        X_train,
-        Y_train,
-        model = model,
-        cat_features = dataset.categoric_features,
-    )
-
-    # run_experiments(
-    #     method,
-    #     individuals = individuals,
-    #     model = model_wrap,
-    #     output_file=f"../results/mlp/{dataset_name}/nice.csv"
-    # );
-
-
-    action_set = get_action_set(dataset, X_train, default_step_size=0.05)
-
-    model_wrap = GeneralClassifier_Shap(
-        model,
-        outlier_detection,
-        X_train,
-        shap_explainer="deep_pipe",
-    )
-
-    #setting multiple criteria
-    range_calc = RangeCalculator(action_set)
-    perc_calc = PercentileCalculator(action_set = action_set)
-
-    def compare_call(pivot):
-        criteria_list = [
-            MaxDistCriterion(
-                pivot,
-                range_calc,
-            ),
-            NumberChangesCriterion(pivot),
-            PercentileCriterion(
-                pivot,
-                perc_calc,
-            )
-        ]
-        return MultiCriterion(criteria_list, pivot)
-
-    print("Running P2CE with multiple objectives with ", dataset_name)
-
-    method = P2CE(
-        action_set = action_set,
-        classifier = model_wrap,
-        compare = compare_call,
-        max_changes = max_changes,
-        outlier_contamination= dataset.outlier_contamination,
-        estimate_outlier=True,
-        time_limit=np.inf,
-    )
-
-    # run_experiments(
-    #     method,
-    #     individuals=individuals,
-    #     model=model_wrap,
-    #     output_file=f"../results/mlp/{dataset}/p2ce_deep_multi.csv"
-    # );
-
-
-    print("Running MAPOCAM with multiple objectives with ", dataset_name)
-
-    for feat in action_set:
-        feat.flip_direction = 1
-        feat.update_grid()
-
-    model_wrap = GeneralClassifier(
-        model,
-        outlier_detection,
-        X_train,
-    )
-
-    #setting multiple criteria
-    range_calc = RangeCalculator(action_set)
-    perc_calc = PercentileCalculator(action_set = action_set)
-
-    def compare_call(pivot):
-        criteria_list = [
-            MaxDistCriterion(
-                pivot,
-                range_calc,
-            ),
-            NumberChangesCriterion(pivot),
-            PercentileCriterion(
-                pivot,
-                perc_calc,
-            )
-        ]
-        return MultiCriterion(criteria_list, pivot)
-    
-    method = MAPOCAM(
-        action_set = action_set,
-        model = model_wrap,
-        criteria = compare_call,
-        max_changes = max_changes,
-    )
-
+    print(f"Running {method_name} with ", dataset_name)
+    # run experiments
     run_experiments(
         method,
         individuals=individuals,
         model=model_wrap,
-        output_file=f"../results/mlp/{dataset_name}/mapocam_multi.csv"
-    );
-
-    print( "Running DICE with multiple counterfactuals with ", dataset_name)
-
-    n_cfs = 4
-
-
-    method = Dice(
-        X_train,
-        Y_train,
-        model,
-        n_cfs = n_cfs,
-        mutable_features = dataset.mutable_features,
-        continuous_features = dataset.continuous_features,
-    )
-
-    run_experiments(
-        method,
-        individuals = individuals,
-        model = model_wrap,
-        output_file=f"../results/mlp/{dataset_name}/dice_multi.csv"
+        output_file=f"../results/mlp/{dataset}/{method_name}.csv",
     )
 
 
@@ -232,19 +198,32 @@ def run(dataset_name, n_samples=200):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dataset_id",
+        "--idx",
         type=int,
         default=0,
-        help="ID of the dataset to use (0: german, 1: taiwan, 2: adult)",
+        help="idx",
     )
     parser.add_argument(
         "--n_samples",
         type=int,
-        default=1_000,
+        default=500,
         help="Number of samples to use from the dataset",
     )
-    datasets = ["german", "taiwan", "adult"]
+    datasets = ["german", "taiwan", "adult", "acsincome", "homecredit"]
+    methods = [
+        "p2ce_deep_abs_diff",
+        "mapocam_deep_abs_diff",
+        "nice",
+        "dice",
+        "p2ce_deep_multi",
+        "mapocam_deep_multi",
+        "dice_multi",
+    ]
     args = parser.parse_args()
-    dataset_name = datasets[args.dataset_id]
+    dataset_id = args.idx // len(methods)
+    method_id = args.idx % len(methods)
 
-    run(dataset_name, n_samples=args.n_samples)
+    dataset_name = datasets[dataset_id]
+    method_name = methods[method_id]
+
+    run(dataset_name, method_name, n_samples=args.n_samples)
